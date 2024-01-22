6e2b40e16547c0b9b7f79c7f970b90ba410abff4
veneta13 <venkireva80@gmail.com>
2024-01-22 10:58:08 +0200

    Add login

---
+++ scripts/login.js
@@ -0,0 +1,15 @@
+const login = () => {
+    event.preventDefault()
+
+    const formData = new FormData(document.getElementById('login_form'));
+    fetch('../php/login.php',  {method:'POST', body:formData})
+        .then(r=>r.json())
+        .then(r=> {
+            console.log("Fetch response:", r.result);
+            if (r.result) {
+                location.replace("./profile.html")
+            } else {
+                window.alert('Грешка при вход, моля, проверете данните и опитайте отново!');
+            }
+        });
+};
