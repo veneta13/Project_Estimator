20dd956e35eeaaf497c5a7e88a36ca5c9926f96f
veneta13 <venkireva80@gmail.com>
2024-02-05 08:56:17 +0200

    Add refresh

--- scripts/manual_estimation.js
+++ scripts/manual_estimation.js
@@ -87,8 +87,6 @@ const saveProject = () => {
 };
 
 const saveTask = () => {
-    event.preventDefault();
-
     const formData = new FormData(document.getElementById('task_form'));
 
     fetch('../php/manual_estimation.php', {method: 'POST', body: formData})
