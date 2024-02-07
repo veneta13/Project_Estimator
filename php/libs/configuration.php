3e3e5740f6a9d9dc3f1fc84eaf7ee95a473ce46a
veneta13 <venkireva80@gmail.com>
2024-02-07 22:58:28 +0200

    Update configuration file

--- php/libs/configuration.php
+++ php/libs/configuration.php
@@ -3,5 +3,5 @@
 define('DBHOST', '127.0.0.1');
 define('DBPORT', '3306');
 define('DBNAME', 'project_db');
-define('USERNAME', 'my_test_admin');
-define('USERPASSWORD', 'testing');
+define('USERNAME', 'root');
+define('USERPASSWORD', '');
