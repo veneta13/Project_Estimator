3b116e4dbf61c946336e4098e13e7c0c00b94e98
veneta13 <venkireva80@gmail.com>
2024-02-07 18:38:15 +0200

    Add configuration file

--- php/libs/Db.php
+++ php/libs/Db.php
@@ -1,16 +1,18 @@
 <?php
 
+require_once "configuration.php";
+
 class Db
 {
     private $connection;
 
     public function __construct()
     {
-        $dbhost = "127.0.0.1";
-        $port = "3306";
-        $dbName = "project_db";
-        $userName = "my_test_admin";
-        $userPassword = "testing";
+        $dbhost = DBHOST;
+        $port = DBPORT;
+        $dbName = DBNAME;
+        $userName = USERNAME;
+        $userPassword = USERPASSWORD;
 
         $this->connection = new PDO("mysql:host=$dbhost; port=$port; dbname=$dbName", $userName, $userPassword,
             [
---
+++ php/libs/configuration.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DBHOST', '127.0.0.1');
+define('DBPORT', '3306');
+define('DBNAME', 'project_db');
+define('USERNAME', 'my_test_admin');
+define('USERPASSWORD', 'testing');
